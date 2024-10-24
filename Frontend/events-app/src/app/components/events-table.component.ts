e12737b73a950e0fa1b18cfc0c2356c7d0e4cfc6
J-Tajheria <jordantajheria@gmail.com>
2024-10-24 18:30:57 +0100

    Add readme and tidy files

--- Frontend/events-app/src/app/components/events-table.component.ts
+++ Frontend/events-app/src/app/components/events-table.component.ts
@@ -8,8 +8,14 @@ import { HttpClient } from '@angular/common/http';
   standalone: true,
   imports: [CommonModule, FormsModule],
   template: `
-    <div>
-      <h1>HELLOOOOOO</h1>
+    <div class="container">
+      <h1 class="events-header">Console Management System</h1>
+      <h2 class="events-header"> Add a new item to the system </h2>
+      <div>
+        <input [(ngModel)]="newEventName" placeholder="Event Name "/>
+        <input [(ngModel)]="newEventStatus" placeholder="Event Status "/>
+        <button (click)="addEvent()"> Add </button>
+      </div>
       <table>
         <thead>
           <tr>
@@ -23,14 +29,20 @@ import { HttpClient } from '@angular/common/http';
             <td>{{ event.eventsName }}</td>
             <td>{{ event.status }}</td>
             <td>{{ event.created_at }}</td>
+            <td>
+              <img src="trash-2.svg" alt="Trashbin" (click)="deleteEvent(event.id)"/>
+            </td>
           </tr>
         </tbody>
       </table>
-    </div>  `,
-  styles: ``
+    </div>
+  `,
+  styleUrl: './events-table.components.css'
 })
 export class EventsTableComponent {
   events: any[] = [];
+  newEventName: string = '';
+  newEventStatus: string = '';
 
   constructor(private http: HttpClient){
     this.loadEvents();
@@ -39,6 +51,28 @@ export class EventsTableComponent {
   loadEvents() {
     this.http.get<any[]>('http://127.0.0.1:5000/events').subscribe(events => {
       this.events = events;
+    });
+  }
+
+  addEvent() {
+    const newEvent = {
+      eventsName: this.newEventName,
+      status: this.newEventStatus,
+      created_at: new Date().toISOString().slice(0, 19).replace('T', ' ')
+    };
+
+    this.http.post('http://127.0.0.1:5000/events', newEvent).subscribe(response => {
+      console.log(response);
+      this.loadEvents(); 
+      this.newEventName = ''; 
+      this.newEventStatus = '';
+
+    });
+  }
+
+  deleteEvent(id: number) {
+    this.http.delete(`http://127.0.0.1:5000/events/${id}`).subscribe(() => {
+      this.loadEvents();
     });
   }
 }
